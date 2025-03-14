e136bae322c4d676e2808274d1662f04b236118c
Guilherm46 <Gbgalvao90@gmail.com>
2025-03-13 21:22:16 -0300

    morango

--- Challenge of the first module/021.py
+++ Challenge of the first module/021.py
@@ -1,2 +1,5 @@
 #faça um programa em python que abra e reproduza
 #o audio de um arquivo mp3
+
+from playsound import playsound
+playsound("/home/loop7time/Documents/WhatsApp Ptt 2025-03-12 at 22.21.55.ogg")
