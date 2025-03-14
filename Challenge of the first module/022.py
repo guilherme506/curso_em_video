42bfdda400c425e3c3bd4bfc5b1a65f87754ca09
Guilherm46 <Gbgalvao90@gmail.com>
2025-03-14 14:10:09 -0300

    morango

--- Challenge of the first module/022.py
+++ Challenge of the first module/022.py
@@ -3,3 +3,12 @@
 # Quantas letras ao todo(sem considerar espaços)
 # Quantas letras tem o primeiro nome
 
+f =str(input('Qual é o seu nome completo? '))
+ 
+#f.lower(n)  colocar tudo em minusculo
+
+#analise = len(f) analise a frase
+#f.stip(n) remover os espaços inuteis 
+
+# f.split(n) vai separar cada palavra
+print(f'O seu nome com todo minusculo fica {f.lower()}')
